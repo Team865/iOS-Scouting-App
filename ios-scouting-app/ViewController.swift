d2c122fb5bbbf3d9238f3597257845f147f6deb7
Pham Duc Loc <phamducloc2003@gmail.com>
2020-02-21 23:31:10 -0500

    Initial Commit

---
+++ ios-scouting-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-scouting-app
+//
+//  Created by DUC LOC on 2/21/20.
+//  Copyright © 2020 Warp7. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
