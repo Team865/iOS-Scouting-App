c7ba69cb3d60abb0e994b3b55f4c4d833e0a06ac
Pham Duc Loc <phamducloc2003@gmail.com>
2020-03-14 06:11:15 -0400

    add board selection add alert for scout to input their name change some icons

--- ios-scouting-app/ViewController.swift
+++ ios-scouting-app/ViewController.swift
@@ -10,10 +10,11 @@ import UIKit
 class ViewController: UIViewController {
 @IBOutlet weak var matchTable: UITableView!
 var matches : [matchSchedule] = []
-
-var numbers = [1,2,3,4,5]
-
+    
 var validMatchNumber : [Int] = []
+    
+var selectedBoard = "B1"
+var scoutName = "First L"
 
 var listOfMatches = [Matches]()
     
@@ -21,7 +22,8 @@ let settingsView = UIViewController()
     
     let settingsTag = 1;
     let additemTag = 2;
-    let boardSelectionTag = 3;
+    let editNameTag = 3;
+    let boardSelectionTag = 4;
 
 override func viewDidLoad() {
     super.viewDidLoad()
@@ -46,10 +48,85 @@ override func viewDidLoad() {
         if(srcObj.tag == additemTag){
             print("Add item")
         }
+        if(srcObj.tag == editNameTag){
+            let alert = UIAlertController(title: "Enter name", message: "Initial and Last", preferredStyle: .alert)
+            alert.addTextField {
+                (UITextField) in UITextField.placeholder = "First L"
+            }
+            
+            let getName = UIAlertAction(title: "OK", style: .default){
+                [weak alert] (_) in
+                let textField = alert?.textFields![0]
+                self.scoutName = textField!.text ?? ""
+                self.updateBoard(board: self.selectedBoard, scout: self.scoutName)
+            }
+            
+            alert.addAction(getName)
+            
+            self.present(alert, animated: true, completion: nil)
+        }
         if(srcObj.tag == boardSelectionTag){
             let alert = UIAlertController(title: "Select board", message: "", preferredStyle: .alert)
-            let OKAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-            alert.addAction(OKAction)
+            
+            let B1 = UIAlertAction(title : "B1", style: .default) {
+                (ACTION) in self.updateBoard(board: "B1", scout: self.scoutName)
+                self.selectedBoard = "B1"
+                self.matchTable.reloadData()
+
+            }
+            let B2 = UIAlertAction(title : "B2", style: .default){
+                (ACTION) in self.updateBoard(board: "B2", scout: self.scoutName)
+                self.selectedBoard = "B2"
+                self.matchTable.reloadData()
+            }
+            let B3 = UIAlertAction(title : "B3", style: .default){
+                (ACTION) in self.updateBoard(board: "B3", scout: self.scoutName)
+                self.selectedBoard = "B3"
+                self.matchTable.reloadData()
+
+            }
+            let R1 = UIAlertAction(title : "R1", style: .default){
+                (ACTION) in self.updateBoard(board: "R1", scout: self.scoutName)
+                self.selectedBoard = "R1"
+                self.matchTable.reloadData()
+
+            }
+            let R2 = UIAlertAction(title : "R2", style: .default){
+                (ACTION) in self.updateBoard(board: "R2", scout: self.scoutName)
+                self.selectedBoard = "R2"
+                self.matchTable.reloadData()
+
+            }
+            let R3 = UIAlertAction(title : "R3", style: .default){
+                (ACTION) in self.updateBoard(board: "R3", scout: self.scoutName)
+                self.selectedBoard = "R3"
+                self.matchTable.reloadData()
+
+            }
+
+            let BX = UIAlertAction(title: "BX", style: .default){
+                (ACTION) in self.updateBoard(board: "BX", scout: self.scoutName)
+                self.selectedBoard = "BX"
+                self.matchTable.reloadData()
+
+            }
+            let RX = UIAlertAction(title: "RX", style: .default){
+                (ACTION) in self.updateBoard(board: "RX", scout: self.scoutName)
+                self.selectedBoard = "RX"
+                self.matchTable.reloadData()
+
+            }
+            
+            alert.addAction(B1)
+            alert.addAction(B2)
+            alert.addAction(B3)
+            alert.addAction(R1)
+            alert.addAction(R2)
+            alert.addAction(R3)
+            alert.addAction(RX)
+            alert.addAction(BX)
+            
+
             self.present(alert, animated: true, completion: nil)
         }
     }
@@ -75,28 +152,76 @@ override func viewDidLoad() {
         }.resume()
     }
     
-    private func setupNavigationBar(){
+    
+    
+    private func updateBoard(board : String, scout : String){
+        let selectedBoard = UITextField(frame: .init(x: 0, y: 0, width: 34, height: 34))
+        selectedBoard.isUserInteractionEnabled = false
+        selectedBoard.text = board
+        
+        let scoutName = UITextField(frame: .init(x: 0, y: 0, width: 34, height: 34))
+        scoutName.isUserInteractionEnabled = false
+        scoutName.text = scout
+        self.navigationItem.leftBarButtonItems = [UIBarButtonItem(customView : self.createSelectBoardButton()), UIBarButtonItem(customView: selectedBoard), UIBarButtonItem(customView: self.createEditNameButton()), UIBarButtonItem(customView: scoutName)]
+    }
+    
+    private func createSelectBoardButton() -> UIButton{
+        let selectBoardButton = UIButton(type : .system)
+        selectBoardButton.setImage(UIImage (named : "paste")?.withRenderingMode(.alwaysOriginal), for: .normal)
+        selectBoardButton.frame = CGRect(x : 0, y : 0, width : 34, height : 34)
+        selectBoardButton.tag = self.boardSelectionTag
+        selectBoardButton.addTarget(self, action: #selector(self.clickHandler(srcObj:)), for: .touchUpInside)
+        
+        return selectBoardButton
+    }
+    
+    private func createEditNameButton() -> UIButton{
+        let editNameButton = UIButton(type : .system)
+        editNameButton.setImage(UIImage(named : "users")?.withRenderingMode(.alwaysOriginal), for: .normal)
+        editNameButton.tag = self.editNameTag
+        editNameButton.addTarget(self, action: #selector(self.clickHandler(srcObj:)), for: .touchUpInside)
+        editNameButton.frame = CGRect(x : 0, y : 0, width : 34, height : 34)
+
+        return editNameButton
+    }
+    
+    private func rightSideButtonsNavBar() -> [UIButton]{
+        var listOfButtons : [UIButton] = []
+        
         let addIconButton = UIButton(type: .system)
-        addIconButton.setImage(UIImage(named: "addIcon")?.withRenderingMode(.alwaysOriginal), for: .normal)
+        addIconButton.setImage(UIImage(named: "addicon")?.withRenderingMode(.alwaysOriginal), for: .normal)
         addIconButton.frame = CGRect(x : 0, y : 0, width : 34, height : 34)
         
         let settingButton = UIButton(type: .system)
         settingButton.setImage(UIImage(named : "settingsIcon")?.withRenderingMode(.alwaysOriginal), for: .normal)
         
-        let selectBoardButton = UIButton(type : .system)
-        selectBoardButton.setImage(UIImage (named : "paste")?.withRenderingMode(.alwaysOriginal), for: .normal)
-        
-        navigationItem.rightBarButtonItems = [UIBarButtonItem(customView: settingButton), UIBarButtonItem(customView: addIconButton)]
-        
-        navigationItem.leftBarButtonItems = [UIBarButtonItem(customView: selectBoardButton)]
+        addIconButton.addTarget(self, action: #selector(self.clickHandler(srcObj:)), for: .touchUpInside)
+        settingButton.addTarget(self, action: #selector(self.clickHandler(srcObj:)), for: .touchUpInside)
         
         addIconButton.tag = self.additemTag
         settingButton.tag = self.settingsTag
-        selectBoardButton.tag = self.boardSelectionTag
         
-        addIconButton.addTarget(self, action: #selector(self.clickHandler(srcObj:)), for: .touchUpInside)
-        settingButton.addTarget(self, action: #selector(self.clickHandler(srcObj:)), for: .touchUpInside)
-        selectBoardButton.addTarget(self, action: #selector(self.clickHandler(srcObj:)), for: .touchUpInside)
+        listOfButtons.append(addIconButton)
+        listOfButtons.append(settingButton)
+        
+        return listOfButtons
+        
+    }
+    
+    private func setupNavigationBar(){
+        
+        let selectedBoard = UITextField(frame: .init(x: 0, y: 0, width: 34, height: 34))
+        selectedBoard.isUserInteractionEnabled = false
+        selectedBoard.text = "B1"
+        
+        let scoutName = UITextField(frame : .init(x : 0, y : 0, width: 34, height: 34))
+        scoutName.isUserInteractionEnabled = false
+        scoutName.text = "First L"
+        
+        navigationItem.rightBarButtonItems = [UIBarButtonItem(customView: rightSideButtonsNavBar()[0]), UIBarButtonItem(customView: rightSideButtonsNavBar()[1])]
+        
+        navigationItem.leftBarButtonItems = [UIBarButtonItem(customView: self.createSelectBoardButton()), UIBarButtonItem(customView: selectedBoard), UIBarButtonItem(customView: self.createEditNameButton()), UIBarButtonItem(customView: scoutName)]
+
     }
     
     func createMatchSchedule() -> [matchSchedule]{
@@ -123,7 +248,7 @@ override func viewDidLoad() {
         
                                     self.listOfMatches[u].alliances.red.team_keys[p] = String(parsedRed)
                                 }
-                                let match = matchSchedule(icon : UIImage(named : "database")!, matchNumber: String(i), blue1: self.listOfMatches[u].alliances.blue.team_keys[0], blue2: self.listOfMatches[u].alliances.blue.team_keys[1],
+                                let match = matchSchedule(icon : UIImage(named : "layers")!, matchNumber: String(i), blue1: self.listOfMatches[u].alliances.blue.team_keys[0], blue2: self.listOfMatches[u].alliances.blue.team_keys[1],
                                                           blue3: self.listOfMatches[u].alliances.blue.team_keys[2],
                                                           red1: self.listOfMatches[u].alliances.red.team_keys[0],
                                                           red2: self.listOfMatches[u].alliances.red.team_keys[1],
@@ -149,9 +274,85 @@ extension ViewController : UITableViewDataSource, UITableViewDelegate
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "Alliances") as! matchScheduleCells
         cell.setMatch(match: match)
-        //cell.blue1.textColor = UIColor.blue
+        
+        switch self.selectedBoard {
+        case "B1":
+            cell.blue1.textColor = UIColor.blue
+            cell.blue2.textColor = UIColor.black
+            cell.blue3.textColor = UIColor.black
+            
+            cell.red1.textColor = UIColor.black
+            cell.red2.textColor = UIColor.black
+            cell.red3.textColor = UIColor.black
+
+        case "B2":
+            cell.blue1.textColor = UIColor.black
+            cell.blue2.textColor = UIColor.blue
+            cell.blue3.textColor = UIColor.black
+            
+            cell.red1.textColor = UIColor.black
+            cell.red2.textColor = UIColor.black
+            cell.red3.textColor = UIColor.black
+        case "B3":
+            cell.blue1.textColor = UIColor.black
+            cell.blue2.textColor = UIColor.black
+            cell.blue3.textColor = UIColor.blue
+            
+            cell.red1.textColor = UIColor.black
+            cell.red2.textColor = UIColor.black
+            cell.red3.textColor = UIColor.black
+        case "R1":
+            cell.blue1.textColor = UIColor.black
+            cell.blue2.textColor = UIColor.black
+            cell.blue3.textColor = UIColor.black
+            
+            cell.red1.textColor = UIColor.red
+            cell.red2.textColor = UIColor.black
+            cell.red3.textColor = UIColor.black
+        case "R2":
+            cell.blue1.textColor = UIColor.black
+            cell.blue2.textColor = UIColor.black
+            cell.blue3.textColor = UIColor.black
+            
+            cell.red1.textColor = UIColor.black
+            cell.red2.textColor = UIColor.red
+            cell.red3.textColor = UIColor.black
+            
+        case "R3":
+            cell.blue1.textColor = UIColor.black
+            cell.blue2.textColor = UIColor.black
+            cell.blue3.textColor = UIColor.black
+        
+            cell.red1.textColor = UIColor.black
+            cell.red2.textColor = UIColor.black
+            cell.red3.textColor = UIColor.red
+            
+        case "RX":
+            cell.red1.textColor = UIColor.red
+            cell.red2.textColor = UIColor.red
+            cell.red3.textColor = UIColor.red
+            
+            cell.blue1.textColor = UIColor.black
+            cell.blue2.textColor = UIColor.black
+            cell.blue3.textColor = UIColor.black
+            
+        case "BX":
+            cell.blue1.textColor = UIColor.blue
+            cell.blue2.textColor = UIColor.blue
+            cell.blue3.textColor = UIColor.blue
+        
+        default:
+            cell.blue1.textColor = UIColor.black
+            cell.blue2.textColor = UIColor.black
+            cell.blue3.textColor = UIColor.black
+            cell.red1.textColor = UIColor.black
+            cell.red2.textColor = UIColor.black
+            cell.red3.textColor = UIColor.black
+}
+        
         return cell
-    }
+       
 }
 
 
+}
