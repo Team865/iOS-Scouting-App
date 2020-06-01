4bfd77dd5a16e034c94c9dc70245796025099c14
Pham Duc Loc <phamducloc2003@gmail.com>
2020-06-01 14:30:34 -0400

    Rework comment options (Make it looks more like an alert)

--- ios-scouting-app/UI/UIExtensions/CustomAlertViewController.swift
+++ ios-scouting-app/UI/UIExtensions/CustomAlertViewController.swift
@@ -11,46 +11,45 @@ import UIKit
 class CustomAlertController : UIViewController {
     var scoutingActivity = ScoutingActivity()
     let stackView = UIStackView()
+    let label = UILabel()
     let textField = UITextField()
+    
     let textFieldContainer = UIView()
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.configureStackView()
+        self.view.backgroundColor = UIColor.black.withAlphaComponent(0.7)
+        self.view.isOpaque = false
         
-        self.stackView.addArrangedSubview(self.textFieldContainer)
+        self.configureStackView()
         configureTextField()
         
         for i in 0..<self.scoutingActivity.commentOptions.count{
             let checkbox = CheckBoxField()
             checkbox.setUpView(data: self.scoutingActivity.commentOptions[i])
-            checkbox.backgroundColor = UIColor.white
-            checkbox.onTimerStarted()
-            checkbox.layer.borderWidth = 1
-            checkbox.layer.cornerRadius = 4
-            checkbox.layer.borderColor = UIColor.blue.cgColor
+            checkbox.configureCheckBoxForCommentOptions()
             self.stackView.addArrangedSubview(checkbox)
-            checkbox.checkBox.titleLabel?.sizeToFit()
         }
         
+        configureCommentTextField()
         self.stackView.addArrangedSubview(self.configureButtonsStackView())
-        
         
     }
     
     func configureTextField()  {
-        self.textField.placeholder = "Enter a comment"
+        self.label.text = "Additional Info"
+        self.label.textAlignment = .left
         
-        self.textFieldContainer.layer.borderColor = UIColor.blue.cgColor
-        self.textFieldContainer.layer.borderWidth = 1
-        self.textFieldContainer.layer.cornerRadius = 4
+        self.textFieldContainer.backgroundColor = UIColor.white
         
-        self.textFieldContainer.addSubview(self.textField)
-        textField.translatesAutoresizingMaskIntoConstraints = false
-        textField.centerYAnchor.constraint(equalTo: self.textFieldContainer.centerYAnchor).isActive = true
-        textField.centerXAnchor.constraint(equalTo: self.textFieldContainer.centerXAnchor).isActive = true
-        textField.widthAnchor.constraint(equalTo: self.textFieldContainer.widthAnchor, multiplier: 0.8).isActive = true
-        textField.heightAnchor.constraint(equalTo: self.textFieldContainer.heightAnchor, multiplier: 0.8).isActive = true
+        self.stackView.addArrangedSubview(self.textFieldContainer)
+        
+        self.textFieldContainer.addSubview(self.label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.centerYAnchor.constraint(equalTo: self.textFieldContainer.centerYAnchor).isActive = true
+        label.centerXAnchor.constraint(equalTo: self.textFieldContainer.centerXAnchor).isActive = true
+        label.widthAnchor.constraint(equalTo: self.textFieldContainer.widthAnchor, multiplier: 0.8).isActive = true
+        label.heightAnchor.constraint(equalTo: self.textFieldContainer.heightAnchor, multiplier: 0.8).isActive = true
     }
     
     func configureButton(text : String, color : UIColor, tag : Int) -> UIButton{
@@ -59,9 +58,7 @@ class CustomAlertController : UIViewController {
         button.titleLabel?.font = button.titleLabel?.font.withSize(CGFloat(Double(UIScreen.main.bounds.height) * 0.025))
         button.setTitleColor(color, for: .normal)
         button.tag = tag
-        button.layer.borderWidth = 1
-        button.layer.borderColor = color.cgColor
-        button.layer.cornerRadius = 4
+        button.backgroundColor = UIColor.white
         button.addTarget(self, action: #selector(actionHandler(sender:)), for: .touchUpInside)
         return button
     }
@@ -72,7 +69,7 @@ class CustomAlertController : UIViewController {
         stackView.distribution = .fillEqually
         stackView.spacing = 0
         
-        let titles = ["Ok", "Cancel"]
+        let titles = ["OK", "Cancel"]
         let colors = [UIColor.blue, UIColor.red]
         for i in 0..<titles.count{
             stackView.addArrangedSubview(self.configureButton(text: titles[i], color: colors[i], tag: i))
@@ -87,12 +84,48 @@ class CustomAlertController : UIViewController {
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
         stackView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
-        stackView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.9).isActive = true
-        stackView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.8).isActive = true
+        stackView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.4).isActive = true
+        stackView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.9).isActive = true
         
         stackView.axis = .vertical
         stackView.distribution = .fillEqually
-        stackView.spacing = 2.5
+        stackView.spacing = 0
+    }
+    
+    func configureCommentTextField(){
+        let view = UIView()
+        view.backgroundColor = UIColor.white
+        
+        textField.placeholder = "Comments"
+        textField.backgroundColor = UIColor.white
+        
+        let comment = UIImageView()
+        comment.image = UIImage(named: "comments")
+        
+        textField.leftViewMode = .always
+        textField.leftView = comment
+        
+        self.stackView.addArrangedSubview(view)
+        
+        view.addSubview(textField)
+        
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        textField.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 5).isActive = true
+        textField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.95).isActive = true
+        textField.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.9).isActive = true
+        
+        let borderLine = UIView()
+        borderLine.backgroundColor = UIColor.black
+        
+        view.addSubview(borderLine)
+        
+        borderLine.translatesAutoresizingMaskIntoConstraints = false
+        borderLine.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 5).isActive = true
+        borderLine.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.95).isActive = true
+        borderLine.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.01).isActive = true
+        borderLine.topAnchor.constraint(equalTo: textField.bottomAnchor).isActive = true
+        borderLine.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
     }
     
     @objc func actionHandler(sender : UIButton){
